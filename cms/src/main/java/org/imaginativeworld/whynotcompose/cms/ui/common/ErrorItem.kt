50c3148c2e6ae9f6bb295b96cc9971582cebaef6
Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2023-06-19 11:04:15 +0600

    Apply spotless

--- cms/src/main/java/org/imaginativeworld/whynotcompose/cms/ui/common/ErrorItem.kt
+++ cms/src/main/java/org/imaginativeworld/whynotcompose/cms/ui/common/ErrorItem.kt
@@ -1,3 +1,29 @@
+/*
+ * Copyright 2023 Md. Mahmudul Hasan Shohag
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Project: Why Not Compose!
+ * Developed by: @ImaginativeShohag
+ *
+ * Md. Mahmudul Hasan Shohag
+ * imaginativeshohag@gmail.com
+ *
+ * Source: https://github.com/ImaginativeShohag/Why-Not-Compose
+ */
+
 package org.imaginativeworld.whynotcompose.cms.ui.common
 
 import android.content.res.Configuration
@@ -14,13 +40,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.imaginativeworld.whynotcompose.cms.theme.CMSAppTheme
-import org.imaginativeworld.whynotcompose.common.compose.theme.AppTheme
 
 @Preview
 @Composable

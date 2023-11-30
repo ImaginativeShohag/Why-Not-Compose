f318e0b6a9004fc3b356f82386fd341d47fb4656
Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2023-12-01 00:13:10 +0600

    Apply spotless

--- app/src/main/java/org/imaginativeworld/whynotcompose/ui/screens/tutorial/navdatapass/NavDataPassThreeScreen.kt
+++ app/src/main/java/org/imaginativeworld/whynotcompose/ui/screens/tutorial/navdatapass/NavDataPassThreeScreen.kt
@@ -45,7 +45,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.imaginativeworld.whynotcompose.common.compose.compositions.AppComponent

b883db5ad43005d89dd153b01fc39e772efc4af6
Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2024-06-05 01:50:15 +0600

    refactor: Apply spotless

--- app/src/main/java/org/imaginativeworld/whynotcompose/ui/screens/tutorial/deeplinks/DeepLinksActivity.kt
+++ app/src/main/java/org/imaginativeworld/whynotcompose/ui/screens/tutorial/deeplinks/DeepLinksActivity.kt
@@ -55,7 +55,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.view.WindowCompat
 import org.imaginativeworld.whynotcompose.base.utils.UIThemeController
 import org.imaginativeworld.whynotcompose.common.compose.compositions.AppComponent
 import org.imaginativeworld.whynotcompose.common.compose.theme.AppTheme

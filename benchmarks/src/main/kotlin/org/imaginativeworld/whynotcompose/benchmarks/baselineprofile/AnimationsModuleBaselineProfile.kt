4c4bddfc1d421b91e03aaa5a4647c9f33e008d4e
Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2024-12-29 19:20:09 +0600

    build: update actions

--- benchmarks/src/main/kotlin/org/imaginativeworld/whynotcompose/benchmarks/baselineprofile/AnimationsModuleBaselineProfile.kt
+++ benchmarks/src/main/kotlin/org/imaginativeworld/whynotcompose/benchmarks/baselineprofile/AnimationsModuleBaselineProfile.kt
@@ -1,106 +1,106 @@
-// /*
-// * Copyright 2024 Md. Mahmudul Hasan Shohag
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// *
-// * ------------------------------------------------------------------------
-// *
-// * Project: Why Not Compose!
-// * Developed by: @ImaginativeShohag
-// *
-// * Md. Mahmudul Hasan Shohag
-// * imaginativeshohag@gmail.com
-// *
-// * Source: https://github.com/ImaginativeShohag/Why-Not-Compose
-// */
-//
-// package org.imaginativeworld.whynotcompose.benchmarks.baselineprofile
-//
-// import androidx.benchmark.macro.junit4.BaselineProfileRule
-// import androidx.test.ext.junit.runners.AndroidJUnit4
-// import androidx.test.filters.LargeTest
-// import androidx.test.platform.app.InstrumentationRegistry
-// import androidx.test.uiautomator.By
-// import org.imaginativeworld.whynotcompose.benchmarks.commonModuleTraverseActions
-// import org.imaginativeworld.whynotcompose.benchmarks.startActivityAndAllowNotifications
-// import org.imaginativeworld.whynotcompose.benchmarks.waitAndFindObject
-// import org.junit.Rule
-// import org.junit.Test
-// import org.junit.runner.RunWith
-//
-// /**
-// * This test class generates a basic startup baseline profile for the target package.
-// *
-// * We recommend you start with this but add important user flows to the profile to improve their performance.
-// * Refer to the [baseline profile documentation](https://d.android.com/topic/performance/baselineprofiles)
-// * for more information.
-// *
-// * You can run the generator with the "Generate Baseline Profile" run configuration in Android Studio or
-// * the equivalent `generateBaselineProfile` gradle task:
-// * ```
-// * ./gradlew :app:generateReleaseBaselineProfile
-// * ```
-// * The run configuration runs the Gradle task and applies filtering to run only the generators.
-// *
-// * Check [documentation](https://d.android.com/topic/performance/benchmarking/macrobenchmark-instrumentation-args)
-// * for more information about available instrumentation arguments.
-// *
-// * After you run the generator, you can verify the improvements running the [org.imaginativeworld.whynotcompose.benchmarks.startup.StartupBenchmarks] benchmark.
-// *
-// * When using this class to generate a baseline profile, only API 33+ or rooted API 28+ are supported.
-// *
-// * The minimum required version of androidx.benchmark to generate a baseline profile is 1.2.0.
-// **/
-// @RunWith(AndroidJUnit4::class)
-// @LargeTest
-// class AnimationsModuleBaselineProfile {
-//
-//    @get:Rule
-//    val rule = BaselineProfileRule()
-//
-//    @Test
-//    fun generate() {
-//        // The application id for the running build variant is read from the instrumentation arguments.
-//        rule.collect(
-//            packageName = InstrumentationRegistry.getArguments().getString("targetAppId")
-//                ?: throw Exception("targetAppId not passed as instrumentation runner arg"),
-//
-//            // See: https://d.android.com/topic/performance/baselineprofiles/dex-layout-optimizations
-//            includeInStartupProfile = true
-//        ) {
-//            // This block defines the app's critical user journey. Here we are interested in
-//            // optimizing for app startup. But you can also navigate and scroll through your most important UI.
-//
-//            // Start default activity for your app
-//            pressHome()
-//            startActivityAndAllowNotifications()
-//
-//            // TODO Write more interactions to optimize advanced journeys of your app.
-//            // For example:
-//            // 1. Wait until the content is asynchronously loaded
-//            // 2. Scroll the feed content
-//            // 3. Navigate to detail screen
-//
-//            // Check UiAutomator documentation for more information how to interact with the app.
-//            // https://d.android.com/training/testing/other-components/ui-automator
-//
-//            // Wait for home screen
-//            device.waitAndFindObject(By.res("screen:home"), 5_000)
-//
-//            commonModuleTraverseActions(
-//                moduleButtonText = "Animations",
-//                screenTag = "screen:animations:index"
-//            )
-//        }
-//    }
-// }
+/*
+* Copyright 2024 Md. Mahmudul Hasan Shohag
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* ------------------------------------------------------------------------
+*
+* Project: Why Not Compose!
+* Developed by: @ImaginativeShohag
+*
+* Md. Mahmudul Hasan Shohag
+* imaginativeshohag@gmail.com
+*
+* Source: https://github.com/ImaginativeShohag/Why-Not-Compose
+*/
+
+package org.imaginativeworld.whynotcompose.benchmarks.baselineprofile
+
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import org.imaginativeworld.whynotcompose.benchmarks.commonModuleTraverseActions
+import org.imaginativeworld.whynotcompose.benchmarks.startActivityAndAllowNotifications
+import org.imaginativeworld.whynotcompose.benchmarks.waitAndFindObject
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * This test class generates a basic startup baseline profile for the target package.
+ *
+ * We recommend you start with this but add important user flows to the profile to improve their performance.
+ * Refer to the [baseline profile documentation](https://d.android.com/topic/performance/baselineprofiles)
+ * for more information.
+ *
+ * You can run the generator with the "Generate Baseline Profile" run configuration in Android Studio or
+ * the equivalent `generateBaselineProfile` gradle task:
+ * ```
+ * ./gradlew :app:generateReleaseBaselineProfile
+ * ```
+ * The run configuration runs the Gradle task and applies filtering to run only the generators.
+ *
+ * Check [documentation](https://d.android.com/topic/performance/benchmarking/macrobenchmark-instrumentation-args)
+ * for more information about available instrumentation arguments.
+ *
+ * After you run the generator, you can verify the improvements running the [org.imaginativeworld.whynotcompose.benchmarks.startup.StartupBenchmarks] benchmark.
+ *
+ * When using this class to generate a baseline profile, only API 33+ or rooted API 28+ are supported.
+ *
+ * The minimum required version of androidx.benchmark to generate a baseline profile is 1.2.0.
+ **/
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AnimationsModuleBaselineProfile {
+
+    @get:Rule
+    val rule = BaselineProfileRule()
+
+    @Test
+    fun generate() {
+        // The application id for the running build variant is read from the instrumentation arguments.
+        rule.collect(
+            packageName = InstrumentationRegistry.getArguments().getString("targetAppId")
+                ?: throw Exception("targetAppId not passed as instrumentation runner arg"),
+
+            // See: https://d.android.com/topic/performance/baselineprofiles/dex-layout-optimizations
+            includeInStartupProfile = true
+        ) {
+            // This block defines the app's critical user journey. Here we are interested in
+            // optimizing for app startup. But you can also navigate and scroll through your most important UI.
+
+            // Start default activity for your app
+            pressHome()
+            startActivityAndAllowNotifications()
+
+            // TODO Write more interactions to optimize advanced journeys of your app.
+            // For example:
+            // 1. Wait until the content is asynchronously loaded
+            // 2. Scroll the feed content
+            // 3. Navigate to detail screen
+
+            // Check UiAutomator documentation for more information how to interact with the app.
+            // https://d.android.com/training/testing/other-components/ui-automator
+
+            // Wait for home screen
+            device.waitAndFindObject(By.res("screen:home"), 5_000)
+
+            commonModuleTraverseActions(
+                moduleButtonText = "Animations",
+                screenTag = "screen:animations:index"
+            )
+        }
+    }
+}
